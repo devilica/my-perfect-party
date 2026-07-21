5be02549d71f45fec61cb36b74095aa2d010a246
MilicaGalic <m.galic@paj-gps.de>
2026-07-21 14:10:12 +0200

    changes ad

--- constants/ads.ts
+++ constants/ads.ts
@@ -1,6 +1,6 @@
-export const ADMOB_APP_ID = 'ca-app-pub-5452343649745884~9366452184';
+export const ADMOB_APP_ID = 'ca-app-pub-5483258571608850~3245032438';
 
-export const BOTTOM_BANNER_UNIT_ID = 'ca-app-pub-5452343649745884/4114125503';
+export const BOTTOM_BANNER_UNIT_ID = 'ca-app-pub-5483258571608850/5089311153';
 
 /** Approximate height of anchored adaptive banner + safe area clearance for scroll padding */
 export const BANNER_CLEARANCE = 60;
