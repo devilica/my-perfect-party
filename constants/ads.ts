70b0e94adac44925d322bc0fa05132197b6b541f
MilicaGalic <m.galic@paj-gps.de>
2026-07-22 12:28:24 +0200

    enable ads

--- constants/ads.ts
+++ constants/ads.ts
@@ -1,5 +1,5 @@
 /** Postavi na false da ugasi sve reklame (banner + rewarded) u cijeloj app */
-export const ADS_ENABLED = false;
+export const ADS_ENABLED = true;
 
 export const ADMOB_APP_ID = 'ca-app-pub-5452343649745884~9366452184';
 
